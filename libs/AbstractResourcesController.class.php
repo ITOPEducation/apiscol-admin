5025144e879393ccb501fb1c9dd0553b0b67f4e0
Joachim Dornbusch <joachim.dornbusch@agroparistech.fr>
2015-09-25 05:46:48 +0200

    Probleme fixed : alert message after deletion of void resource

--- libs/AbstractResourcesController.class.php
+++ libs/AbstractResourcesController.class.php
@@ -1,67 +1,64 @@
 <?php
 abstract class AbstractResourcesController implements IController {
-
 	protected $view;
 	protected $model;
 	protected $mainController;
 	protected $prefix;
-
 	public function __construct($mainController, $model, $prefix) {
-		$this->mainController=$mainController;
+		$this->mainController = $mainController;
 		$this->model = $model;
-		$this->prefix=$prefix;
+		$this->prefix = $prefix;
 	}
-
 	public function registerMetadataId() {
-		if (isset (Security :: $_CLEAN['metadata-id'])) {
+		if (isset ( Security::$_CLEAN ['metadata-id'] )) {
 			try {
-				$this->model->setMetadataId(Security :: $_CLEAN['metadata-id']);
-
-			} catch (HttpRequestException $e) {
-				$this->mainController->setInError(true);
-				//TODO traduire
-				$this->mainController->setErrorMessage("Problèmes de connexion à ApiScol pour obtenir les détails de la ressource.", $e->getContent());
-			} catch (BadUrlRequestException $e) {
-				$this->mainController->setInError(true);
-				//TODO traduire
-				$this->mainController->setErrorMessage("Pas de réponse d'ApiScol pour les détails de la ressource. Le service est peut-être arrêté ou en panne.", $e->getMessage());
-			} 
+				$this->model->setMetadataId ( Security::$_CLEAN ['metadata-id'] );
+			} catch ( HttpRequestException $e ) {
+				$this->mainController->setInError ( true );
+				// TODO traduire
+				$this->mainController->setErrorMessage ( "Problèmes de connexion à ApiScol pour obtenir les détails de la ressource.", $e->getContent () );
+			} catch ( BadUrlRequestException $e ) {
+				$this->mainController->setInError ( true );
+				// TODO traduire
+				$this->mainController->setErrorMessage ( "Pas de réponse d'ApiScol pour les détails de la ressource. Le service est peut-être arrêté ou en panne.", $e->getMessage () );
+			}
 		}
 	}
-	public function acquireContent() {
+	public function acquireContent($throwErrorIfNoContent = true) {
 		try {
-			if(!$this->model->getMetadata()->isPackage())
-				$this->model->acquireContentRepresentation();
-			else $this->model->acquirePackRepresentation();
-			if(!$this->model->getMetadata()->isPackage())
+			if (! $this->model->getMetadata ()->isPackage ())
+				$this->model->acquireContentRepresentation ();
+			else
+				$this->model->acquirePackRepresentation ();
+			if (! $this->model->getMetadata ()->isPackage ())
 				try {
-				$this->model->acquireContentThumbRepresentation();
-			} catch (HttpRequestException $e) {
-				$this->mainController->setInError(true);
-				$this->mainController->setErrorMessage("Problèmes de connexion à ApiScol pour obtenir les suggestions de miniatures basées sur le contenu.", $e->getContent());
-			} catch (BadUrlRequestException $e) {
-				$this->mainController->setInError(true);
-				$this->mainController->setErrorMessage("Problèmes de connexion à ApiScol pour obtenir les suggestions de miniatures basées sur le contenu.", $e->getMessage());
+					$this->model->acquireContentThumbRepresentation ();
+				} catch ( HttpRequestException $e ) {
+					$this->mainController->setInError ( true );
+					$this->mainController->setErrorMessage ( "Problèmes de connexion à ApiScol pour obtenir les suggestions de miniatures basées sur le contenu.", $e->getContent () );
+				} catch ( BadUrlRequestException $e ) {
+					$this->mainController->setInError ( true );
+					$this->mainController->setErrorMessage ( "Problèmes de connexion à ApiScol pour obtenir les suggestions de miniatures basées sur le contenu.", $e->getMessage () );
+				}
+		} catch ( HttpRequestException $e ) {
+			$this->mainController->setInError ( true );
+			$this->mainController->setErrorMessage ( "Problèmes de connexion à ApiScol pour obtenir le contenu de la ressource.", $e->getContent () );
+		} catch ( MetadataWithoutContentException $e ) {
+			if (true === $throwErrorIfNoContent) {
+				$this->mainController->setInError ( true );
+				$this->mainController->setErrorMessage ( "Aucun contenu n'est associé à cette metadonnée.", $e->getMessage () );
 			}
-		} catch (HttpRequestException $e) {
-			$this->mainController->setInError(true);
-			$this->mainController->setErrorMessage("Problèmes de connexion à ApiScol pour obtenir le contenu de la ressource.", $e->getContent());
-		} catch (MetadataWithoutContentException $e) {
-			$this->mainController->setInError(true);
-			$this->mainController->setErrorMessage("Aucun contenu n'est associé à cette metadonnée.", $e->getMessage());
-		} catch (BadUrlRequestException $e) {
-			$this->mainController->setInError(true);
-			$this->mainController->setErrorMessage("Problèmes de connexion à ApiScol pour obtenir le contenu de la ressource.", $e->getMessage());
-		} catch (CorruptedXMLStringException $e ) {
-			$this->mainController->setInError(true);
-			$this->mainController->setErrorMessage("Le contenu de cette métadonnée est illisible.", $e->getMessage());
+		} catch ( BadUrlRequestException $e ) {
+			$this->mainController->setInError ( true );
+			$this->mainController->setErrorMessage ( "Problèmes de connexion à ApiScol pour obtenir le contenu de la ressource.", $e->getMessage () );
+		} catch ( CorruptedXMLStringException $e ) {
+			$this->mainController->setInError ( true );
+			$this->mainController->setErrorMessage ( "Le contenu de cette métadonnée est illisible.", $e->getMessage () );
 		}
 	}
 	public function acquireFacetsTest() {
-		$this->model->acquireFacetsTest();
+		$this->model->acquireFacetsTest ();
 	}
-
-
 	public function getView() {
 		return $this->view;
 	}
